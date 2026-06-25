7141523a1ad8e32eed03c71be481e8c363886820
Axis-HikaruMurakami <h_murakami@axis-net.co.jp>
2026-06-25 17:22:47 +0900

    ほぼしゅうりょう！！

--- src/main/java/main/AdminHome.java
+++ src/main/java/main/AdminHome.java
@@ -11,13 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import been.Asset;
-import been.Display;
 import been.Location;
 import been.User;
 import dao.AdminDisplayDao;
-import dao.Displaydao;
-import dao.Userdao;
 
 @WebServlet("/AdminHome")
 public class AdminHome extends HttpServlet {
@@ -39,7 +35,7 @@ public class AdminHome extends HttpServlet {
 		} else {
 			HttpSession session = request.getSession(false);
 		    User user = (User) session.getAttribute("user");
-
+		    
 		    // ログインユーザーの location_cd
 		    String userLocation_id = user.getLocation_cd();
 		   
@@ -62,47 +58,6 @@ public class AdminHome extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String 	id 			= request.getParameter("id");
-		String 	password 	= request.getParameter("password");
-		User 	user 		= Userdao.login(id, password);
-		
-		
-		if(user!=null) {
-
-		    // ★ ログイン後に location_cd を取得
-		    String location_cd = Userdao.getLocation_cd(user.getUser_id());
-		    user.setLocation_cd(location_cd);
-			
-			List<Display> displayList = Displaydao.listdisplay(location_cd);
-			
-			request.setAttribute("displayList", displayList);
-			
-			
-			List<Asset> assetname = Displaydao.assetName();
-			
-			request.setAttribute("assetname", assetname);
-			
-			
-			//成功時セッションを保存してmain.jspへ
-			HttpSession session1 = request.getSession();
-			session1.setAttribute("user", user);
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("displayList", displayList);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/main.jsp");
-			rd.forward(request, response);
-			
-		} else {
-			//nullの場合はログインエラーでログイン画面に遷移
-		    request.setAttribute("error", "true");
-			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
-			rd.forward(request, response);
-		}
-		
-		
-		
-		
 		
 	}
 
